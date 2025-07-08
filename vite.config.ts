d36a58cd5ccb79fca1154b8dec19c025b1aa686d
mosi <mosshengqi@outlook.com>
2025-07-08 16:39:15 +0800

    update

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,14 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base:
+    process.env.NODE_ENV === "production"
+      ? "/7702/" // 注意结尾斜杠
+      : "/",
+  build: {
+    assetsDir: "static", // 统一资源目录
+    manifest: true, // 生成资源映射表
+  },
   server: {
     port: 3000,
     proxy: {
